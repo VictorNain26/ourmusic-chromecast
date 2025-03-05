0af9c5c3175dfa039179a82d4740bd564be71c2d
Victor <victor.lenain26@gmail.com>
2025-03-05 13:48:25 +0100

    update heartbeat

--- app/layout.js
+++ app/layout.js
@@ -21,51 +21,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* Chargement du SDK Cast Receiver */}
+        {/* Chargement du SDK Cast Receiver avant l'interactivité */}
         <Script
           src="https://www.gstatic.com/cast/sdk/libs/caf_receiver/v3/cast_receiver_framework.js"
           strategy="beforeInteractive"
         />
-        {/* Initialisation du Cast Receiver et envoi des messages heartbeat et keep-alive */}
-        <Script id="cast-init-and-keepalive" strategy="beforeInteractive">
+        {/* Initialisation du Cast Receiver après l'interactivité */}
+        <Script id="cast-init" strategy="afterInteractive">
           {`
-            window.addEventListener('load', function() {
-              if (typeof cast !== "undefined" && cast.framework) {
-                const context = cast.framework.CastReceiverContext.getInstance();
-                context.setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
-                context.setInactivityTimeout(6000); // Timeout étendu à 100 minutes
-                context.start();
-                console.log("Cast Receiver initialisé.");
-
-                // Fonction d'envoi du heartbeat
-                const sendHeartbeat = () => {
-                  context.sendCustomMessage(
-                    "urn:x-cast:com.google.cast.sample.heartbeat",
-                    undefined,
-                    { type: "HEARTBEAT" }
-                  );
-                  console.log("Heartbeat envoyé");
-                };
-
-                // Fonction d'envoi du keep-alive
-                const sendKeepAlive = () => {
-                  context.sendCustomMessage(
-                    "urn:x-cast:com.google.cast.sample.keepalive",
-                    undefined,
-                    { type: "KEEP_ALIVE" }
-                  );
-                  console.log("Keep-alive envoyé");
-                };
-
-                // Envoi des messages toutes les 10 secondes
-                setInterval(() => {
-                  sendHeartbeat();
-                  sendKeepAlive();
-                }, 10000);
-              } else {
-                console.error("Le SDK Cast n'est pas disponible.");
+            (function initCast() {
+              function startCast() {
+                if (typeof cast !== "undefined" && cast.framework) {
+                  const context = cast.framework.CastReceiverContext.getInstance();
+                  context.setLoggerLevel(cast.framework.LoggerLevel.DEBUG);
+                  context.start();
+                  console.log("Cast Receiver initialisé.");
+                } else {
+                  console.error("Le SDK Cast n'est pas disponible.");
+                }
               }
-            });
+
+              // Vérification de l'état du document
+              if (document.readyState === 'complete') {
+                startCast();
+              } else {
+                window.addEventListener('load', startCast);
+              }
+            })();
           `}
         </Script>
       </head>
